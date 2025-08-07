63b0eb3c6be60e77f3efb459488b8d4f65c33364
SIlverlakeD <michael@silverlakedigital.com>
2025-08-07 11:16:54 -0400

    Update post_build.sh

--- post_build.sh
+++ post_build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-# post_build.sh
-
-# Log the start of browser install
-echo "Running post_build.sh to install Playwright browsers..."
-
-# Install the Playwright browsers (Chromium, Firefox, WebKit)
+echo "Installing Playwright browsers"
 npx playwright install --with-deps
